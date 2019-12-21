7f8ff548df591d3b3613c621ff56814c660e6d02
hkoketsu <hiroki.koketsu@gmail.com>
2019-12-21 12:43:02 -0800

    Change site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,13 @@
 
 module.exports = {
   siteMetadata: {
-    title: 'gatsby-starter-typescript-plus',
-    description: 'A starter kit for TypeScript-based Gatsby projects with sensible defaults.',
-    keywords: 'gatsbyjs, gatsby, javascript, sample, something',
-    siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com',
+    title: 'hkoketsu',
+    description: 'A portfolio of Hiroki Koketsu.',
+    siteUrl: 'http://localhost:8000',
     author: {
-      name: 'Resi Respati',
-      url: 'https://twitter.com/resir014',
-      email: 'resir014@gmail.com'
+      name: 'Hiroki Koketsu',
+      url: 'https://www.linkedin.com/in/hkoketsu/',
+      email: 'hiroki.koketsu@gmail.com'
     }
   },
   plugins: [
