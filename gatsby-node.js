74235160c9d6b5e110fc3bec54e5430efa8a8174
hkoketsu <hiroki.koketsu@gmail.com>
2019-12-24 17:06:55 -0800

    Update gatsby-cli and add hot loader

--- gatsby-node.js
+++ gatsby-node.js
@@ -83,3 +83,13 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+
+exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
+  const config = getConfig()
+  if (stage.startsWith('develop') && config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react-dom': '@hot-loader/react-dom'
+    }
+  }
+}
