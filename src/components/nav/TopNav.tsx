7f9867bdb9f323c1279a42c2b2f15c52e00bf7d5
hkoketsu <hiroki.koketsu@gmail.com>
2019-12-26 20:15:27 -0800

    Remove right margin from top bar

--- src/components/nav/TopNav.tsx
+++ src/components/nav/TopNav.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import styled from '@emotion/styled'
 import NavItem from './NavItem'
-import { Link } from 'gatsby'
 
 const TopNav: React.FC = () => (
   <StyledTopNav>
@@ -23,7 +22,6 @@ const StyledTopNav = styled.nav`
 const StyledList = styled.ul`
   height: 100%;
   padding: 10px;
-  margin-right: 50px;
   display: flex;
   align-items: center;
   justify-content: flex-end;
